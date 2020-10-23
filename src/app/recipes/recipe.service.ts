1d9d7201b74a07cb93026726887f9eb1ba4b6a8c
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-23 17:47:19 +0530

    Recipe service configured

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import {Recipe} from './recipe.model';
 
 export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
         
