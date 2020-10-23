d507a45e0c6c45355d85e00b0e8357e1a92c1d15
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-23 14:12:14 +0530

    Provide recipe service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]//me component eke and meke child components wala RecipeService
+  //eke same instance eka use karanna pluwan
 })
 export class RecipesComponent implements OnInit {
 
