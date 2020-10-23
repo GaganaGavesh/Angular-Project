14fc9b4239ffd0533e79cd5b907bbb4756dd5bb6
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-23 14:13:41 +0530

    Remove recipes  details & get from resipe service

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 import {Recipe} from '../recipe.model';
 
@@ -13,34 +14,37 @@ export class RecipesListComponent implements OnInit {
   //e kiyannne mm kalin hadapu recipe modele eke tyna class eke obi eke type eka
   //methana thama apata ona obj eka create karanne constructor ekata data yawala
   //recipes kiyanne array ekak,Recipe object thama e array eke tynne
-  recipes: Recipe[] = [
-    //Recipe class bluprint eken obj ekak hadanawa
-    //new keyword eka dala
-    // new Recipe('A test Recipe 1', 
-    //            'This is simply a test 1', 
-    //            'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
-    new Recipe('A test Recipe 1', 
-                'This is simply a test 1', 
-                'assets/coffee.jpg'),
+  // recipes: Recipe[] = [
+  //   //Recipe class bluprint eken obj ekak hadanawa
+  //   //new keyword eka dala
+  //   // new Recipe('A test Recipe 1', 
+  //   //            'This is simply a test 1', 
+  //   //            'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
+  //   new Recipe('A test Recipe 1', 
+  //               'This is simply a test 1', 
+  //               'assets/coffee.jpg'),
 
-    new Recipe('A test Recipe 2', 
-               'This is simply a test 2', 
-               'assets/karapincha.jpg'),
+  //   new Recipe('A test Recipe 2', 
+  //              'This is simply a test 2', 
+  //              'assets/karapincha.jpg'),
 
-    new Recipe('A test Recipe 3', 
-               'This is simply a test 3', 
-               'assets/item3.jpg'),
+  //   new Recipe('A test Recipe 3', 
+  //              'This is simply a test 3', 
+  //              'assets/item3.jpg'),
 
-    new Recipe('A test Recipe 4', 
-               'This is simply a test 4', 
-               'assets/item4.webp')
-  ];
+  //   new Recipe('A test Recipe 4', 
+  //              'This is simply a test 4', 
+  //              'assets/item4.webp')
+  // ];
+
+  recipes: Recipe[] = [];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelrcted(recipe: Recipe){
