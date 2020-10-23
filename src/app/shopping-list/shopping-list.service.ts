f4a18c8c14b1c90453fc640e49566b30b16761af
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-23 17:43:58 +0530

    Shopping list popolated

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
 export class ShoppingListService{
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
     
+        {name: 'orange', amount: 25}
+      ];
+
+    getIngredients(){
+        return this.ingredients;
+    }
+    
+    onIngredientAdded(ingredient: Ingredient){
+        this.ingredients.push(ingredient);//slice eken ena ekata thama push wennee
+        this.ingredientsChanged.emit(this.ingredients.slice())
+      }
+
 }
