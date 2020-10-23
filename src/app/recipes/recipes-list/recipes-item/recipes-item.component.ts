cf47ae93128af2432885ab302e89b0ca51969167
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-23 17:45:35 +0530

    Component bind with the recipe service

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipes-item',
@@ -8,14 +9,19 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipesItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+  //@Output() recipeSelected = new EventEmitter<void>();
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
   }
 
   onSelected(){
-    this.recipeSelected.emit();
+    //this.recipeSelected.emit();//event emit kalama meken parent ekata eken eke parent yanna ona
+    //eka diga process ekak,so recipe service eka use karanna hadanne.eken lesiyenma centralize 
+    //karanna pluwn me long process eka
+
+    //recipe service ekekn evint emit karanna emit method call eka
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
