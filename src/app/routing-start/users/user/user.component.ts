1bef9d62e14e3717a42f92757098b144540bba3d
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-25 09:13:23 +0530

    Configured router link with subscribe params

--- src/app/routing-start/users/user/user.component.ts
+++ src/app/routing-start/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  //user: {id: number, name: string};
+  user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  //ActiveRoute kiyanne json ekak eke tynawa godak meta data
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name:this.route.snapshot.params['name']
+    };
+    //updated parameters observer karagannawa user object eka update karanawa
+    //danata tyna state eka change karaganna ona nathnam me subscribe method eka add karannna ona ne
+    //snapshot method eken yanna pluwan
+    this.route.params
+    .subscribe(
+      (params: Params)=>{
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      });
   }
 
 }
