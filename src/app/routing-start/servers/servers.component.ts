7ee9bf6ab988b1aa92e2a4e231d782bf9c9f01e0
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-25 09:11:32 +0530

    Routing injected

--- src/app/routing-start/servers/servers.component.ts
+++ src/app/routing-start/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -10,10 +11,18 @@ export class ServersComponent implements OnInit {
 
   servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+  onReload(){
+    //this.router.navigate(['srevers'],{relativeTo: this.route});
+    //mokakr hari path ekakata ralatively route karanna pluwan
+    //ActivatedRoute kiyana eke ambanak route details tynawa
   }
 
 }
