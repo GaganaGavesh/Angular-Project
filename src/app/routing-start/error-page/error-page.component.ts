d54853c1c7528ef35c33d1bd6d7fbd4cc25f13d2
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-27 08:01:11 +0530

    static error content added

---
+++ src/app/routing-start/error-page/error-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+
+@Component({
+  selector: 'app-error-page',
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.css']
+})
+export class ErrorPageComponent implements OnInit {
+
+  erroeMessage: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    //this.erroeMessage = this.route.snapshot.data['message'];
+    this.route.data.subscribe(
+      (data: Data)=>{
+        this.erroeMessage = data['message'];
+      }
+    )
+  }
+
+}
