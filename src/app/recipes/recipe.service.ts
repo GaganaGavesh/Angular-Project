0772303366b6a525180e3e7b79e5dbffdaac68f6
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-28 17:00:07 +0530

    getRecipe method implemented to get recipe by ID

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -57,4 +57,8 @@ export class RecipeService{
       addIngredientsToShoppingList(ingredients: Ingredient[]){
         this.shoppingListService.addIngredients(ingredients);
       }
+
+      getRecipe(index: number){
+        return this.recipes[index];
+      }
 }
