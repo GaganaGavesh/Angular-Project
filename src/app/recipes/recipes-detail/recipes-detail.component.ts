26620ac03d23768809e52c91ed608e66447bc637
Gagana Gavesh <gaveshgamage@gmail.com>
2020-10-28 16:56:26 +0530

    Edit recipe butten functionalities added

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,4 +1,6 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,15 +11,33 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipesDetailComponent implements OnInit {
 
+  //before routing
+  // @Input() selectedRecipeDetail: Recipe;
 
-  @Input() selectedRecipeDetail: Recipe;
+  //after routing
+  selectedRecipeDetail: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    //const id = this.route.snapshot.params['id'];//meka load wena welawe eka witharai ganne//
+    //recipe changes walata react karanna ona nisa anith krame danna ona
+
+    this.route.params.subscribe(
+      (params: Params)=>{
+        this.id = +params['id'];
+        this.selectedRecipeDetail = this.recipeService.getRecipe(this.id)
+      }
+    )
   }
 
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.selectedRecipeDetail.ingredients);
+  }
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo: this.route});
+    //this.router.navigate(['../',this.id,'edit'],{relativeTo: this.route})//eka level ekak passata ghilla
+    //id eka add karala ilangata edit kalla add karanawa
   }
 }
