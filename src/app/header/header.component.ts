1d35e414b5b2068f5a8caede74eafec684fff33f
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-09 12:28:05 +0530

    Click listner method implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core'
 
+import { DataStoregeService } from '../shared/data-storage.service';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
 
+    constructor(private dataStorageService: DataStoregeService){}
     //@Output() featureSelected = new EventEmitter<string>();
 
     onClickFeature(feature: string){
         //this.featureSelected.emit(feature);
+    }
+
+    onSavedata(){
+        this.dataStorageService.storeRecipies();
+
     }
 
 }
