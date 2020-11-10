920f2d63a1b27aa918374c53941592e0831684b5
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-10 20:18:08 +0530

    Auth component route added

--- src/app/recipe-routing.module.ts
+++ src/app/recipe-routing.module.ts
@@ -10,16 +10,19 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipeStartComponent},//recipe component eka athule thama pennanne
         {path: 'new', component: RecipeEditComponent},
-        {path: ':id', component: RecipesDetailComponent, resolve: [RecipesResolverService]},
+        {path: ':id', component: RecipesDetailComponent, resolve: [RecipesResolverService]},//resolver eka dala tyna 
+        //path open wenakota resolver eka run wela thama url eka open wenne
         {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]},
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
+    {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
