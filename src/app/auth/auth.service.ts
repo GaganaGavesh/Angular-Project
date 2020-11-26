f752923569df2fcefe1fd59184c6ad5118093fd2
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-26 22:53:29 +0530

    user subject added. next it in login and signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,11 @@
 //This service is for sign users in and up, Manage the token
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';//aluth observable ekak create karanawa error eka wrap karala
+import { catchError,tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';//aluth observable ekak create karanawa error eka wrap karala
 //rxjs aniwaryen observable ekak return karanawa
+
+import { User } from './user.model';
 
 //responce data wala contain wenne me interface eke tyna data tika kiyala POST request 1ta danna thama meka haduwe
 export interface AuthResponseData{
@@ -22,6 +24,9 @@ export interface AuthResponseData{
 //mko Http Client kiyana service eka mekata ganna nisa
 //service ekakata service ekak inject karanakota aniwa @Injectable danna ona
 export class AuthService {
+
+    user = new Subject<User>();//aluth user emit karaanwaa, meka user ona wena thanakadi ganna ahaki
+
     constructor(private http : HttpClient){}//meka thama service eka
 
     signUp(email: String, password: String){
@@ -33,6 +38,7 @@ export class AuthService {
         //e generic type danna pluwan POST resuest responce ekata eken ena responce, e tika api danna nisa 
         //e tika hariyatama mehemai kiyala interface ekak hadala , me type eke responce ekak thama aniwa enne kiyala 
         //kiyanawa
+        //POST eken enne responce ekai error ekai thama, e observable eke tynne me deka
        return this.http.post<AuthResponseData>
        ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBeNFjlXDZ7YT8VCJNrYh5VKynXft3KZ0Y',
       
@@ -41,7 +47,21 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleResponse));
+        ).pipe(catchError(this.handleResponse),tap(resData=>{
+            this.handleAuthentication(
+                resData.email, 
+                resData.localId, 
+                resData.idToken, 
+                +resData.expiresIn)
+            // const expirationDate = new Date(new Date().getTime() + +resData.expiresIn*1000);
+            // //toke time eka current time ekata ekathu karala date ekakata wrap karanawa ethakota apata complete timestamp ekak 
+            // //ganna pluwn expirationdate kiyana const ekata
+            // const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
+            // //dan api user details aragena tynne ilangata karanne user event ekak emit karanawa Subject kiyana eken 
+            // //methanin karanne Emit karana eka, next kalama uda user kiyana subject eken eit wenawa aluth user
+
+            // this.user.next(user);//emit the currently logged in user
+        }));
         //catch error eka error rka catch karagannawa 
         //subscription eke error case ekata thama meka yannee
         //observable eke tyna  me catch karana error rkata karana magul tika karala ayemath me observable ekatama wrap
@@ -60,10 +80,32 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true 
-            }).pipe(catchError(this.handleResponse));
+            }).pipe(catchError(this.handleResponse),tap(resData=>{
+                this.handleAuthentication(
+                    resData.email, 
+                    resData.localId, 
+                    resData.idToken, 
+                    +resData.expiresIn)
+                })
+        );
     }
     //meken return karanne observable ekak
     //eka thawa spesify karanawa me observable eken ena data "AuthResponseData" type ekee data thamai kiyala
+
+    private handleAuthentication(email: String, userId: String,token: String, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn*1000);
+            //toke time eka current time ekata ekathu karala date ekakata wrap karanawa ethakota apata complete timestamp ekak 
+            //ganna pluwn expirationdate kiyana const ekata
+            const user = new User(
+                email, 
+                userId, 
+                token, 
+                expirationDate);
+            //dan api user details aragena tynne ilangata karanne user event ekak emit karanawa Subject kiyana eken 
+            //methanin karanne Emit karana eka, next kalama uda user kiyana subject eken eit wenawa aluth user
+
+            this.user.next(user);//emit the currently logged in user
+    }
 
     private handleResponse(errorRes: HttpErrorResponse){
         //ERROR HANDLING LOGIC
