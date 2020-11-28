ca72037b135d59e3fb7f704185705ba7c3efc7cc
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-29 00:07:20 +0530

    user subscription and unsubscription added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core'
+import { Subscription } from 'rxjs';
 
+import { AuthService } from '../auth/auth.service';
 import { DataStoregeService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit, OnDestroy{
 
-    constructor(private dataStorageService: DataStoregeService){}
+    isAuthenticated = false;
+    private userSub: Subscription;
+
+    constructor(private dataStorageService: DataStoregeService, private authService: AuthService){}
     //@Output() featureSelected = new EventEmitter<string>();
+
+    ngOnInit(){
+        //methana api karanne,api auth eke hadala tyna user subject eken user kenek create una gaman,
+        //methana api subsctibe karala tyna nisa apita e datra tika ganna ahaki
+
+        //me wage subscribe karana ewa api unsubscribe karaganna ona 
+        //memory leaks ekema nawathinawa ethakota
+        //meken emit karanawa user object ekak
+        this.userSub = this.authService.user.subscribe(user=>{
+            this.isAuthenticated = !user ? false : true;
+            //this.isAuthenticated = !!user;
+            console.log("user "+ user);
+        })
+        
+    }
 
     onClickFeature(feature: string){
         //this.featureSelected.emit(feature);
@@ -26,4 +46,8 @@ export class HeaderComponent{
         this.dataStorageService.fetchRecipies().subscribe();
     }
 
+    ngOnDestroy(){
+        //userSub will be unsubscribed
+        this.userSub.unsubscribe();
+    }
 }
